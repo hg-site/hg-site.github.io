636758a5539634c81665789807dd04d62f1d1f7c
Conor Delahunty <conor@Conors-iMac.local>
2017-07-26 12:10:49 +0200

    Remove link js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,22 +1,22 @@
 // Open external links in new tabs
 
-$(document).ready(function() {
+// $(document).ready(function() {
 
-  var website = window.location.hostname;
+//   var website = window.location.hostname;
 
-  var internalLinkRegex = new RegExp('^((((http:\\/\\/|https:\\/\\/)(www\\.)?)?'
-                                     + website
-                                     + ')|(localhost:\\d{4})|(\\/.*))(\\/.*)?$', '');
+//   var internalLinkRegex = new RegExp('^((((http:\\/\\/|https:\\/\\/)(www\\.)?)?'
+//                                      + website
+//                                      + ')|(localhost:\\d{4})|(\\/.*))(\\/.*)?$', '');
 
-  $('a').filter(function() {
-    var href = $(this).attr('href');
-    return !internalLinkRegex.test(href);
-  })
-  .each(function() {
-    $(this).attr('target', '_blank');
-  });
+//   $('a').filter(function() {
+//     var href = $(this).attr('href');
+//     return !internalLinkRegex.test(href);
+//   })
+//   .each(function() {
+//     $(this).attr('target', '_blank');
+//   });
 
-});
+// });
 
 // Make the table headings sort using listjs
 
